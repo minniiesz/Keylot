32feefe7815ad2ed8acc61460658e6f8dc51243b
wittawat pongpipat <wittawat0001@gmail.com>
2022-02-05 00:29:08 +0700

    fix bug null case

--- KeylotV1/Form1.cs
+++ KeylotV1/Form1.cs
@@ -18,7 +18,13 @@ namespace KeylotV1
 {
     public partial class Form1 : Form
     {
+        private bool checkEnter = true;
         private bool allowshowdisplay = false;
+        public static Timer timer1 = new Timer
+        {
+            Interval = 10000,
+            Enabled = false
+        };        
         protected override void SetVisibleCore(bool value)
         {
             base.SetVisibleCore(allowshowdisplay ? value : allowshowdisplay);
@@ -26,21 +32,43 @@ namespace KeylotV1
         public static string input = ""; 
         public Form1()
         {
+            timer1.Tick += Timer1_Tick;
             InitializeComponent();
             SetVisibleCore(allowshowdisplay);
-           
+
+
+            Subscribe.subscribeValue();
             _globalKeyboardHook = new GlobalKeyboardHook(new Keys[] { Keys.Divide, Keys.Multiply,Keys.NumPad0, Keys.NumPad1, Keys.NumPad2, Keys.NumPad3, Keys.NumPad4, Keys.NumPad5
                                    ,Keys.NumPad6,Keys.NumPad7,Keys.NumPad9,Keys.Return,Keys.Subtract,Keys.Decimal});
-
+            Porty.test();
+            Porty.sendReady();
             // Hooks into all keys.
             //_globalKeyboardHook = new GlobalKeyboardHook();
             _globalKeyboardHook.KeyboardPressed += OnKeyPressed;
             //PostKeylot();
             
+           
 
         }
 
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (Api.IsJobRunning)
+            {
+                Porty.sendRunningJob();
+            }
+            else
+            {
+                Porty.sendReady();
+            }
+            
+            checkEnter = true;
+            input = "";
+            timer1.Enabled = false;
+        }
+
         private GlobalKeyboardHook _globalKeyboardHook;
+
 
         /*        private void buttonHook_Click(object sender, EventArgs e)
                 {
@@ -56,20 +84,62 @@ namespace KeylotV1
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private  void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e)
+        private void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e)
         {
+            
             // EDT: No need to filter for VkSnapshot anymore. This now gets handled
             // through the constructor of GlobalKeyboardHook(...).
             if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyDown)
             {
+                if (!checkEnter)
+                {
+                    timer1.Enabled = false;
+                    countReady();
+                }
                 // Now you can access both, the key and virtual code
                 Keys loggedKey = e.KeyboardData.Key;
-
                 var text = loggedKey.ToString().ToLower();
                 var key = CheckKey(text);
                 if (text == "return")
-                {
-                        Api.PostKeylot(input);
+
+                {     
+                    if (checkEnter)
+                    {
+                        checkEnter = false;
+                        Porty.sendMessage("set_display", "Key Job Name...", "Then Press Enter");
+                        countReady();
+                    }
+                    else
+                    {
+
+                        if(input == "9999")
+                        { 
+                            if (Api.IsJobRunning)
+                            {
+                                Api.CloseTicket();
+                            }
+                            else
+                            {
+                                Porty.sendReady();
+                            }
+                        }
+                        else 
+                        {
+                            if (!Api.IsJobRunning)
+                            {
+
+
+                                Api.PostKeylot(input); //open ticket   
+                            }
+                            else
+                            {
+                                Porty.sendRunningJob();
+                            }
+                        }
+                        
+                        checkEnter = true;
+                        timer1.Enabled = false;
+                    }
                         input = "";
                         
                 }else if (text == "subtract")
@@ -78,15 +148,24 @@ namespace KeylotV1
                 }
                 else
                 {
-
+                    if (!checkEnter)
+                    {
                         input += key;
+                    }
+
                 }
+
 
                 
                 
             }
             
         }
+        private void countReady()
+        {
+            timer1.Enabled = true;
+        }
+
         private  string CheckKey(string key)
         {
             if (key.Contains("numpad"))
@@ -112,7 +191,9 @@ namespace KeylotV1
 
 
         }
-
+        
 
     }
 }
+
+
